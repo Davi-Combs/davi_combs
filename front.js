81ea1d78727f8aa5fe6403b24af93e97b8596a61
Davi Dawn <93796971+Davi-Combs@users.noreply.github.com>
2025-10-01 19:42:24 -0500

    Update front.js

--- front.js
+++ front.js
@@ -4,7 +4,7 @@ const messagesDiv = document.getElementById("messages");
 const input = document.getElementById("userInput");
 
 // Change this to your deployed Vercel URL
-const API_URL = "https://alpacatheai.vercel.app/api/chat";
+/*const API_URL = "https://alpacatheai.vercel.app/api/chat";*/
 
 
 form.addEventListener("submit", async (e) => {
