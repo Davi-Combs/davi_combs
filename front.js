94dcedf2d0da5c9abffc9d3c2be48d55174d4491
Davi Dawn <93796971+Davi-Combs@users.noreply.github.com>
2025-10-01 22:37:59 -0500

    Update front.js

--- front.js
+++ front.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", async (e) => {
   appendMessage("YOU", message);
   input.value = "";
 
-  const res = await fetch("https://YOUR-RAILWAY-APP.up.railway.app/chat", {
+  const res = await fetch("https://davicombs-production.up.railway.app/?/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message })
